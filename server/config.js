d0d36bf9c0e0386d840170effe3d8f5f05f95a2c
Glitch (rapid-sedate-zoo) <none>
2024-01-12 20:12:41 +0000

    🐯💈 Checkpoint ./server/config.js:58209495/72

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If 'localhost:NUMBER', the port must equal the port setting.
-    host: "random-arraspstest.glitch.me",
+    host: "rapid-sedate-zoo.glitch.me",
 
     // Which port to run the web server on.
     port: 80,
