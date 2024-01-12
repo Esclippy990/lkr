68248bed5d75da3a5432aa15df1861a403a37011
Glitch (rapid-sedate-zoo) <none>
2024-01-12 19:38:25 +0000

    🏉🛬 Checkpoint ./server/config.js:58209495/35 ./server/permissions.js:58209495/16

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ module.exports = {
     host: "rapid-sedate-zoo.glitch.me",
 
     // Which port to run the web server on.
-    port: 30000,
+    port: 80,
 
     // How often to update the list of the entities that players can see.
     // Has effects of when entities are activated.
